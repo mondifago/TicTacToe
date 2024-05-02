6e347c559a6b1bec281960fa0e2a700762e7750b
ugochukwumaduakor <umaduakor@hotmail.com>
2024-05-03 01:20:51 +1000

    Reduced strategy to methods only with just the program class

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,190 +1,166 @@
 ﻿using System;
-using System.Xml.Linq;
 
-namespace TicTacToe;
-
-public class Program
+namespace TicTacToe
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        char[,] board = {
-            {' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}
-        };
+        const int BOARD_ROW_DIM = 3;
+        const int BOARD_COLUMN_DIM = 3;
+        const int ZERO_BASED_INDEX = 0;
+        const int THIRD_INDEX = 2;
+        const int COL_1 = 1;
+        const int COL_2 = 2;
+        const int COL_3 = 3;
+        const int ROW_1 = 1;
+        const int ROW_2 = 2;
+        const int ROW_3 = 3;
 
-        PrintBoard(board);
-
-        Player1 HumanPlayer = new Player1("Human",'X');
-        Player2 ComputerPlayer = new Player2("Computer",'O');
-        string currentPlayer;
-
-        bool isPlayer1Turn = true;
-
-        while (true)
+        public static void Main(string[] args)
         {
-            currentPlayer = isPlayer1Turn ? HumanPlayer.ToString() : ComputerPlayer.ToString();
-            Console.WriteLine("Player " + (isPlayer1Turn ? "1" : "2") + "'s turn:");
+            string player1 = "Human";
+            string player2 = "Computer";
+            char boardEmptySpace = ' ';
+            char humanSymbol = 'X';
+            char computerSymbol = 'O';
+            
+            char[,] board = new char[BOARD_ROW_DIM,BOARD_COLUMN_DIM];
+            bool isPlayer1Turn = true;
 
-            if (currentPlayer == HumanPlayer.ToString())
-            {
-                Console.WriteLine("Enter row (1-3):");
-                int row = int.Parse(Console.ReadLine()) - 1;
-
-                Console.WriteLine("Enter column (1-3):");
-                int col = int.Parse(Console.ReadLine()) - 1;
-
-                try
+            for (int row = ZERO_BASED_INDEX; row < board.GetLength(0); row++)
+            { 
+                for (int col = ZERO_BASED_INDEX; col < board.GetLength(1); col++)
                 {
-                    HumanPlayer.HumanMove(board, row, col);
+                    board[row, col] = boardEmptySpace;
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    continue;
-                }
-            }
-            if (currentPlayer == ComputerPlayer.ToString())
-            {
-                (int row, int col) = ComputerPlayer.ComputerMove(board);
-                
             }
 
             PrintBoard(board);
-
-            if (CheckWinner(board, currentPlayer[currentPlayer.Length - 1]))
+            
+            while (true)
             {
-                Console.WriteLine("Player " + (isPlayer1Turn ? "1" : "2") + " wins!");
-                break;
-            }
-            else if (IsBoardFull(board))
-            {
-                Console.WriteLine("It's a draw!");
-                break;
-            }
+                string currentPlayer = isPlayer1Turn ? player1 : player2;
+                Console.WriteLine("It is " + (isPlayer1Turn ? player1 : player2) + "'s turn:");
 
-            isPlayer1Turn = !isPlayer1Turn;
-        }
-    }
-
-    public static void PrintBoard(char[,] board)
-    {
-        Console.WriteLine("  1 2 3");
-        Console.WriteLine(" -------");
-        for (int row = 0; row < 3; row++)
-        {
-            Console.Write(row + 1 + "|");
-            for (int col = 0; col < 3; col++)
-            {
-                Console.Write(" " + board[row, col] + " ");
-                if (col < 2)
+                if (currentPlayer == player1)
                 {
-                    Console.Write("|");
+                    Console.Write($"Enter row ({ROW_1}-{ROW_3}):\t");
+                    int row = int.Parse(Console.ReadLine()) - 1;
+
+                    Console.Write($"Enter column ({COL_1}-{COL_3}):\t");
+                    int col = int.Parse(Console.ReadLine()) - 1;
+
+                    try
+                    {
+                        HumanMove(board, row, col, humanSymbol);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        continue;
+                    }
+                }
+                else if (currentPlayer == player2)
+                {
+                    (int row, int col) = ComputerMove(board);
+                    board[row, col] = computerSymbol;
+                }
+
+                PrintBoard(board);
+
+                if (CheckWinner(board, humanSymbol))
+                {
+                    Console.WriteLine(player1 +" wins!");
+                    break;
+                }
+                else if (CheckWinner(board, computerSymbol))
+                {
+                    Console.WriteLine(player2 +" wins!");
+                    break;
+                }
+                else if (IsBoardFull(board))
+                {
+                    Console.WriteLine("It's a draw!");
+                    break;
+                }
+
+                isPlayer1Turn = !isPlayer1Turn;
+            }
+        }
+
+        public static void PrintBoard(char[,] board)
+        {
+            Console.WriteLine($"   {COL_1}   {COL_2}   {COL_3}");
+            Console.WriteLine(" -------------");
+            for (int row = ZERO_BASED_INDEX; row < board.GetLength(0); row++)
+            {
+                Console.Write((row + 1) + "|");
+                for (int col = ZERO_BASED_INDEX; col < board.GetLength(1); col++)
+                {
+                    Console.Write(" " + board[row, col] + " ");
+                    if (col < THIRD_INDEX)
+                    {
+                        Console.Write("|");
+                    }
+                }
+                Console.WriteLine();
+                if (row < THIRD_INDEX)
+                {
+                    Console.WriteLine(" -------------");
                 }
             }
-            Console.WriteLine();
-            if (row < 2)
+        }
+
+        public static void HumanMove(char[,] board, int row, int col, char symbol, char boardEmptySpace = ' ')
+        {
+            if (row < 0 || row >= board.GetLength(0) || col < 0 || col >= board.GetLength(1) || board[row, col] != boardEmptySpace)
             {
-                Console.WriteLine(" -------");
+                throw new InvalidOperationException("Invalid move! Please choose an empty cell.");
             }
-        }
-    }
 
-    public static bool CheckWinner(char[,] board, char symbol)
-    {
-        // Check rows, columns, and diagonals for a winner
-        for (int i = 0; i < 3; i++)
-        {
-            if (board[i, 0] == symbol && board[i, 1] == symbol && board[i, 2] == symbol)
-                return true;
-            if (board[0, i] == symbol && board[1, i] == symbol && board[2, i] == symbol)
-                return true;
+            board[row, col] = symbol;
         }
-        if (board[0, 0] == symbol && board[1, 1] == symbol && board[2, 2] == symbol)
-            return true;
-        if (board[0, 2] == symbol && board[1, 1] == symbol && board[2, 0] == symbol)
-            return true;
-        return false;
-    }
 
-    public static bool IsBoardFull(char[,] board)
-    {
-        foreach (char cell in board)
+        public static (int, int) ComputerMove(char[,] board, char boardEmptySpace = ' ')
         {
-            if (cell == ' ')
+            // Choose a random empty cell
+            Random random = new Random();
+            int row, col;
+            do
             {
-                return false;
+                row = random.Next(ZERO_BASED_INDEX, BOARD_ROW_DIM);
+                col = random.Next(ZERO_BASED_INDEX, BOARD_COLUMN_DIM);
+            } while (board[row, col] != boardEmptySpace);
+
+            return (row, col);
+        }
+
+        public static bool CheckWinner(char[,] board, char symbol)
+        {
+            // Check rows, columns, and diagonals for a winner
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == symbol && board[i, 1] == symbol && board[i, 2] == symbol)
+                    return true;
+                if (board[0, i] == symbol && board[1, i] == symbol && board[2, i] == symbol)
+                    return true;
             }
-        }
-        return true;
-    }
-}
-
-
-
-public class Player1 
-{
-    private string NameOfPlayer1;
-    private char SymbolOfPlayer1;
-
-    public string HumanPlayer { get; set; }
-    public char HumanSymbol { get; set; }
-
-    public Player1(string name, char symbol)
-    {
-        NameOfPlayer1 = name;
-        SymbolOfPlayer1 = symbol;
-    }
-
-    public char HumanMove(char[,] board, int row, int col)
-    {
-        if (row < 0 || row >= board.GetLength(0) || col < 0 || col >= board.GetLength(1) || board[row, col] != ' ')
-        {
-            throw new InvalidOperationException("Invalid move! Please choose an empty cell.");
+            if (board[0, 0] == symbol && board[1, 1] == symbol && board[2, 2] == symbol)
+                return true;
+            if (board[0, 2] == symbol && board[1, 1] == symbol && board[2, 0] == symbol)
+                return true;
+            return false;
         }
 
-        board[row, col] = HumanSymbol;
-        return HumanSymbol;
-    }
-
-    public override string ToString()
-    {
-        return $"HumanPlayer: {HumanPlayer}, HumanSymbol: {HumanSymbol}";
-    }
-}
-
-public class Player2
-{
-    private string NameOfPlayer2;
-    private char SymbolOfPlayer2;
-
-    public string ComputerPlayer { get; set; }
-    public char ComputerSymbol { get; set; }
-
-    public Player2(string name, char symbol)
-    {
-        NameOfPlayer2 = name;
-        SymbolOfPlayer2 = symbol;
-    }
-
-    public (int, int) ComputerMove(char[,] board)
-    {
-        // Choose a random empty cell
-        Random random = new Random();
-        int row, col;
-        do
+        public static bool IsBoardFull(char[,] board, char boardEmptySpace = ' ')
         {
-            row = random.Next(0, 3);
-            col = random.Next(0, 3);
-        } while (board[row, col] != ' ');
-
-        return (row, col);
-    }
-        
-    public override string ToString()
-    {
-        return $"ComputerPlayer: {ComputerPlayer}, ComputerSymbol: {ComputerSymbol}";
+            foreach (char cell in board)
+            {
+                if (cell == boardEmptySpace)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
-
-
